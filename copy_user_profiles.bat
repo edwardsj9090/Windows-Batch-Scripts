28d777735e0b3c047a10b143697f46a8e1664c67
runlevel4 <edwardsj9090@gmail.com>
2020-11-06 22:01:05 -0500

    Update copy_user_profiles.bat

--- copy_user_profiles.bat
+++ copy_user_profiles.bat
@@ -1,6 +1,7 @@
 @echo off
 
-:: FOR loop that copies a file to each user directory
-:: Just replace the "\path\to\filename.ext" with the path to the actual file that you want to copy to each user
+:: FOR loop that copies a file to multiple directories at once
+:: You can edit it to fit your needs.  In my case I needed to copy a file to all user AppData directories (50+ user profiles) at once.
+:: Replace the "\path\to\filename" with the path to the actual file that you want to copy
 
-for /D %%U in ("C:\Users\*") Do xcopy "path\to\filename" "%%U\" /Y
+for /D %%U in ("C:\Users\*") Do xcopy "path\to\filename" "%%U\AppData\Local" /Y
