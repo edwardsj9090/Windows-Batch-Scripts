13fa6406ed49f1b93ed7c572311abfcc94e2bbe6
runlevel4 <edwardsj9090@gmail.com>
2021-03-08 18:22:49 -0500

    Delete getIPConfigAll.bat

--- getIPConfigAll.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-echo This will retrieve the MAC/Physical Hardware address of a computer...
-echo.
-SET /P computerName=Enter Computer Name:
-
-psexec \\%computerName% ipconfig /all
-echo.
-pause
