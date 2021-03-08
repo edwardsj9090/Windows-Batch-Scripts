693035ab3452ac4e5305906ceb243e147dce642d
runlevel4 <edwardsj9090@gmail.com>
2021-03-08 18:26:13 -0500

    Delete restart-print-spooler.bat

--- restart-print-spooler.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-SET /P computerName=Enter Computer Name: 
-
-echo Restarting Print Spooler Service
-psexec \\%computerName% net stop spooler
-pause
-psexec \\%computerName% net start spooler
-pause
