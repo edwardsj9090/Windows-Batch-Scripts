197fde090f48bd0490212736a318f68c2cfee4ef
runlevel4 <edwardsj9090@gmail.com>
2021-08-03 10:26:47 -0400

    Update add-printer.bat

--- remote-scripts/add-printer.bat
+++ remote-scripts/add-printer.bat
@@ -5,6 +5,6 @@ echo.
 SET /P compName=Enter Computer Name:
 SET /P printerName=Enter Printer Name:
 echo.
-psexec \\%compName% printui.exe /ga /q /n"\\fh-ws-print\%printerName%"
+psexec \\%compName% printui.exe /ga /q /n"\\printserver\%printerName%"
 echo.
 pause
